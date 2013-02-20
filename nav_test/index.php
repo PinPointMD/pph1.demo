5c136b9a83b7d54d801ff2e2fd61e43de1f1a5d7
jhartmann-pinpointmd <jhartmann@pinpointmd.com>
2013-02-20 16:34:59 -0600

    new index file for nav_test directory

---
+++ nav_test/index.php
@@ -0,0 +1,27 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>Navagation Tests</h2>
+        <h3>The following links, where available, shall open into separate windows:</h3>
+        <ol>
+            <li><a target="_blank" href="pi_coordinator/home/">PI Coordinator</a></li>
+            <li>Patient Registrar</a></li>
+            <li><a target="_blank" href="nurse_ma/engagement/overview.php">Nurse/Medical Assistant</a></li>
+            <li><a target="_blank" href="physician/engagement/overview.php">Physician</a></li>
+            <li><a target="_blank" href="callc_manager/engagement/overview.php">Call Center Manager</a></li>
+            <li>Call Center Agent</a></li>
+            <li>Basic User</a></li>
+            <li>Admin</a></li>
+            <li>Shared</a></li>
+            <li>Patient</a></li>
+        </ol>
+    </body>
+</html>
